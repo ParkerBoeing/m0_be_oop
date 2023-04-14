e5abdfc6ef13392a40b75801b4d41d44c2001640
Parker Boeing <parker.boeing@gmail.com>
2023-04-13 20:06:38 -0600

    Add a Hobbit class

--- oop.rb
+++ oop.rb
@@ -77,3 +77,27 @@ p drag1
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+class Hobbit
+    def initialize(name, disposition, age = 0)
+        @name = name
+        @disposition = disposition
+        @age = age
+        @is_adult = false
+        @is_old = false
+        @has_ring = false
+            if @name == "Frodo"
+                @has_ring = true
+            end
+    end
+
+    def celebrate_birthday
+        @age += 1
+        if @age >= 33
+            @is_adult = true
+        end
+        
+        if @age >= 101
+            @is_old = true
+        end
+    end
+end
